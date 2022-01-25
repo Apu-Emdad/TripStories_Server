473b584198734d528ab538f2e811c003eff56709
unknown <apuemdad96@gmail.com>
2022-01-26 04:08:51 +0600

    blogs to the server

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
+const ObjectId = require("mongodb").ObjectId;
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://TripStoires_admin:6BqFJWDLaY8q72c9@cluster0.1ikru.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("found client");
+
+    const database = client.db("TripStories");
+    const blogCollection = database.collection("Blogs");
+
+    app.get("/blogs", async (req, res) => {
+      const cursor = blogCollection.find({});
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("hello from my first ever node");
+});
+
+app.listen(port, () => {
+  console.log("listening to port", port);
+});
